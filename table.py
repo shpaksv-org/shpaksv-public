627ad078b56a0cfe2516854f7ed25bca9bbfb664
shpaksv.test <96796070+shpaksv@users.noreply.github.com>
2022-02-18 12:24:24 -0500

    Update table.py

--- table.py
+++ table.py
@@ -1,3 +1,9 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.table(data.iloc[0:10])
+df = pd.DataFrame(
+    np.random.randn(10, 5),
+    columns=('col %d' % i for i in range(5)))
+
+st.table(df)
