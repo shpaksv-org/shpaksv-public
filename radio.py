2cdec67cbce331dded9616a95a0a7d1965ab46f2
shpaksv.test <96796070+shpaksv@users.noreply.github.com>
2022-02-23 18:11:11 -0500

    Update radio.py

--- radio.py
+++ radio.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 radio = st.radio('Selecte your favorite radio', options=('radio ga ga', 'radio', 'tapok'))
 if radio == 'radio ga ga':
-  st.video('youtube.com/watch?v=azdwsXLmrHE')
+  st.video('https://www.youtube.com/watch?v=azdwsXLmrHE')
 elif radio == 'radio':
   st.video('https://www.youtube.com/watch?v=z0NfI2NeDHI')
 else:
