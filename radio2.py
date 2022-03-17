621f70121b0b3dd4595de7d0c185f92eb25fceb6
kostenkohanna <99220470+kostenkohanna@users.noreply.github.com>
2022-03-17 16:38:05 +0300

    Delete radio2.py

--- radio2.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-radio = st.radio('Selecte your favorite radio', options=('radio ga ga', 'radio', 'tapok'))
-if radio == 'radio ga ga':
-  st.video('https://www.youtube.com/watch?v=azdwsXLmrHE')
-elif radio == 'radio':
-  st.video('https://www.youtube.com/watch?v=z0NfI2NeDHI')
-else:
-  st.video('https://www.youtube.com/watch?v=fV8d4Grhej8')
