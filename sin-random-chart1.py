e429e66dd8cf18f7552643b0d55f27afebecccc2
shpaksv.test <96796070+shpaksv@users.noreply.github.com>
2022-03-22 18:51:35 -0400

    Update sin-random-chart1.py

--- sin-random-chart1.py
+++ sin-random-chart1.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 
-t = np.arrange(0.00, 10*np.pi, 0.05)
+t = np.arange(0.00, 10*np.pi, 0.05)
 data = pd.DataFrame({
   "rand": np.random.rand(1, len(t))[0] + 10,
   "sin+rand": np.random.rand(1, len(t))[0] + 3*np.sin(t) + np.sin(t * 0.5)
